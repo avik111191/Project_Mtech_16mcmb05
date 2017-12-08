0ce180d73952b9869c261ea5aaaf1391feb24458
Unknown <avik111191@gmail.com>
2017-12-08 17:50:09 +0530

    spark context setup

--- src/nsga2/app.scala
+++ src/nsga2/app.scala
@@ -1,5 +1,24 @@
 package nsga2
 
+import org.apache.spark.SparkContext 
+import org.apache.spark.SparkContext._ 
+import org.apache.spark._  
+ 
+
+
 object app {
   
+
+  def main(args: Array[String]) {
+        val transactionsIn = args(1)
+        val usersIn = args(0)
+        val conf = new SparkConf().setAppName("SparkJoins").setMaster("local")
+        val conext = new SparkContext(conf)
+        val job = new ExampleJob(context)
+        val results = job.run(transactionsIn, usersIn)
+        val output = args(2)
+        results.saveAsTextFile(output)
+        context.stop()
+  }
+
 }
